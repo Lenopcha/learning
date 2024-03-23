323f7fa558c718f1d33e857270aa6fb928b2f9e8
lenapshinka <Lenopcha@gmail.com>
2024-03-23 16:11:47 +0200

    How to find minimum odd num

--- 2_0_H The smallest Odd Element/The smallest Odd Element.py
+++ 2_0_H The smallest Odd Element/The smallest Odd Element.py
@@ -1,26 +1,12 @@
 my_input = input("Just give me some numbers, separated by spaces: ")
+numbers = [int(x) for x in my_input.split()]
+minimum = int(max(my_input))
 
-minimum = 9999
-number = ""  # Змінна для роботи з поточним для циклу
-
-for i in my_input:
-    if i != " ": #не пробіл (щоб визачити коли кінець циклу)
-        number += i
-    else:
-        if number:
-            num = int(number)
-            if num % 2 != 0 and num < minimum:
-                minimum = num
-            number = ""  #роблю змінну пустою
-
-if number:
-    num = int(number)
+for num in numbers:
     if num % 2 != 0 and num < minimum:
         minimum = num
 
-if minimum != 9999:
+if minimum != int(max(my_input)):  # Якщо мінімум оновлено (знайдено непарне число)
     print(f"The minimum odd number is: {minimum}")
 else:
     print("0")
-
-
