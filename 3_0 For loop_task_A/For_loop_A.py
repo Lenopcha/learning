7d23d38130a5049c6fd91fd962751672ffba8841
lenapshinka <Lenopcha@gmail.com>
2024-04-02 21:00:36 +0300

    Given two integers A and B (while A ≤ B). Print all the numbers from A to B inclusive

--- 3_0 For loop_task_A/For_loop_A.py
+++ 3_0 For loop_task_A/For_loop_A.py
@@ -1,16 +1,17 @@
 
-my_list = [1, 5]
-#1
+start = int(input("Enter the starting value: "))
+end = int(input("Enter the ending value: "))
 
+# 1.
 my_new_list = []
-for i in range(my_list[0], my_list[1]+1):
+for i in range(start, end + 1):
     my_new_list.append(i)
 print(my_new_list)
 
-#2
-my_new_list_2 = list(map(lambda x: x, range(my_list[0], my_list[1] + 1)))
+# 2.
+my_new_list_2 = [x for x in range(start, end + 1)]
 print(my_new_list_2)
 
-#3
-my_new_list_3 = [x for x in range(my_list[0], my_list[1] + 1)]
+# 3.
+my_new_list_3 = list(map(lambda x: x, range(start, end + 1)))
 print(my_new_list_3)
