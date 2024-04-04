f4dd063746a796eceae8b80e64dc3d5c78db3bca
lenapshinka <Lenopcha@gmail.com>
2024-04-04 13:47:49 +0300

    Print the index of the first positive element in the given list.

---
+++ 2_0_I First positive/2_0_I First positive.py
@@ -0,0 +1,7 @@
+my_list = [-3, -2, -1, 0, 1, 2, 3]
+index = 0
+
+# go through list to find a positive element
+while my_list[index] <= 0:
+    index += 1
+print("Index of first positive element:", index)
