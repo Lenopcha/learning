e697226e0ded86dd87993e2727e35eb92c7e0ac3
lenapshinka <Lenopcha@gmail.com>
2024-05-27 19:13:46 +0300

    Voting Function _ new _ Implement the majority voting function major(x, y, z) (denoted as ⟨x,y,z⟩). The function returns the value that occurs most frequently among its arguments

--- 3_O Voting Function/3_O Voting Function.py
+++ 3_O Voting Function/3_O Voting Function.py
@@ -1,14 +1,11 @@
 def major(*args):
-    # Count the occurrences of True
-    true_count = sum(args)
-
-    # Calculate the half length
-    half_length = len(args) / 2
+    # Count the occurrences of True and False
+    true_count = args.count(True)
+    false_count = args.count(False)
 
     # Determine and return the majority value
-    if true_count > half_length:
+    if true_count > false_count:
         return True
-
     return False
 
 
@@ -19,4 +16,3 @@ print(major(False, False, False))  # Output: False
 print(major(True, True, True))  # Output: True
 print(major(True, False, True, False, True))  # Output: True
 print(major(True, False, False, False))  # Output: False
-
