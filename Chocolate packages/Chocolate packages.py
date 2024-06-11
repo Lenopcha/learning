cf74198bb14a683bad017caefb646c03d63b9dd8
lenapshinka <Lenopcha@gmail.com>
2024-06-11 15:18:31 +0300

    There are boxes with chocolate bars, with a random number of bars in each box. We need to repack them to other boxes with a known particular number of bars. Calculate the number of new boxes and how many original boxes will be left. Write a function, the input should have 2 parameters: list of original boxes with the number of bars in each box, number of bars in new boxes
    Example:
    
    list of original boxes:
    2, 3, 1, 5
    
    number of bars in new boxes:
    5
    
    result:
    new boxes: 2, left: 1 box with 1 bar

--- Chocolate packages/Chocolate packages.py
+++ Chocolate packages/Chocolate packages.py
@@ -8,15 +8,25 @@ def repack_boxes(original_boxes, bars_per_new_box):
     # Remaining bars
     remaining_bars = total_bars % bars_per_new_box
 
-    # Leftover boxes
+    # Calculate leftover boxes
     leftover_boxes = []
+    current_bars = remaining_bars
+
     if remaining_bars > 0:
-        leftover_boxes.append(remaining_bars)
+        for box in original_boxes:
+            if box <= current_bars:
+                leftover_boxes.append(box)
+                current_bars -= box
+            else:
+                if current_bars > 0:
+                    leftover_boxes.append(current_bars)
+                break
 
     # Print the result
     leftover_boxes_count = len(leftover_boxes)
+    remaining_bars_list = ', '.join(map(str, leftover_boxes))
     print(
-        f"new boxes: {new_boxes}, left: {leftover_boxes_count} box{'es' if leftover_boxes_count != 1 else ''} with {remaining_bars} bar{'s' if remaining_bars != 1 else ''}")
+        f"new boxes: {new_boxes}, left: {leftover_boxes_count} box{'es' if leftover_boxes_count != 1 else ''} with {remaining_bars_list} bar{'s' if remaining_bars != 1 else ''}")
 
     return new_boxes, leftover_boxes
 
